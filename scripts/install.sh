2a89e46acba9a1bd05ac8b31a57fe99aca1fbdd0
Brian Perry <contact.brianperry@gmail.com>
2020-06-15 13:09:43 -0500

    Added phpunit

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,8 @@
 
 lando start
 lando composer install
+# lando drush site:install minimal --site-name="Olivero Sandbox" -y
 lando drush site:install --site-name="Olivero Sandbox" -y
 lando drush then olivero
 lando drush config-set system.theme default olivero -y
+lando drush upwd admin admin
